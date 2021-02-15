c03952c1154cdc1e4fb16d1d06670ae0b733fdb3
Qi Cao <alanqicao@gmail.com>
2021-02-15 11:33:44 -0700

    resumepage

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,25 +1,76 @@
-import { Grid } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import React from "react";
 import "./Resume.css";
+import resumeData from "../untils/resumeData";
+import CustomTimeLine, { CustomTimeLineSeparator } from "./CustomTimeLine";
+import WorkIcon from "@material-ui/icons/Work";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import SchoolIcon from '@material-ui/icons/School';
+
 
 const Resume = () => {
   return (
     <>
       {/* about me  */}
-      <Grid container className="">
-        <Grid item className='section_title'>
-            <span></span>
-            <Typography>About Me</Typography>
+      <Grid container className="section pb_45">
+        <Grid item className="section_title mb_30">
+          <span> </span>
+          <h6 className="section_title_text">About Me</h6>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body2" className="aboutme_text">
+            {resumeData.about}
+          </Typography>
         </Grid>
       </Grid>
       {/* education and experiences */}
-      <Grid container className=""></Grid>
+      <Grid container className="section">
+        <Grid item className="section_title mb_30">
+          <span> </span>
+          <h6 className="section_title_text">Resume</h6>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container className='resume_timeline'>
+            {/* experiences */}
+            <Grid item sm={12} md={6}>
+              <CustomTimeLine title="Work Experience" icon={<WorkIcon />}>
+                {resumeData.experiences.map((experience) => (
+                  <TimelineItem>
+                    <CustomTimeLineSeparator/>
+                    <TimelineContent className='timeline_content'>
+                      <Typography className='timeline_title'>{experience.title}</Typography>
+                      <Typography variant='caption' className='timeline_date'>{experience.date}</Typography>
+                      <Typography variant='body2' className='timeline_description'>{experience.description}</Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeLine>
+            </Grid>
+            {/* Education */}
+            <Grid item sm={12} md={6}>
+            <CustomTimeLine title="Education" icon={<SchoolIcon />}>
+                {resumeData.educations.map((education) => (
+                  <TimelineItem>
+                    <CustomTimeLineSeparator/>
+                    <TimelineContent className='timeline_content'>
+                      <Typography className='timeline_title'>{education.title}</Typography>
+                      <Typography variant='caption' className='timeline_date'>{education.date}</Typography>
+                      <Typography variant='body2' className='timeline_description'>{education.description}</Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeLine>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
       {/* services */}
-      <Grid container className=""></Grid>
+      <Grid container className="section"></Grid>
       {/* skill */}
-      <Grid container className=""></Grid>
+      <Grid container className="section"></Grid>
       {/* contact */}
-      <Grid container className=""></Grid>
+      <Grid container className="section"></Grid>
     </>
   );
 };
--- src/untils/resumeData.js
+++ src/untils/resumeData.js
@@ -1,30 +1,128 @@
 import React from "react";
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import { WebOutlined } from "@material-ui/icons";
 
-export default{
-  name:'Qi Cao (Alan)',
-  title:'Full stack developer',
-  phone:'801-557-3216',
-  email:'alanqicao@gmail.com',
-  web:'https://www.alanqicao.com',
-  address:'Salt Lake City, UT',
+export default {
+  name: "Qi Cao (Alan)",
+  title: "Full stack developer",
+  phone: "801-557-3216",
+  email: "alanqicao@gmail.com",
+  web: "https://www.alanqicao.com",
+  address: "Salt Lake City, UT",
 
-  socials:{
-
-    Github:{
-      link:'https://github.com/alanqicao',
-      text:'MyGithub',
-      icon:<GitHubIcon/>,
+  socials: {
+    Github: {
+      link: "https://github.com/alanqicao",
+      text: "MyGithub",
+      icon: <GitHubIcon />,
     },
 
-    LinkIn:{
-        link:'https://linkedin.com/in/alanqicao',
-        text:'MyLinkedin',
-        icon:<LinkedInIcon/>,
-    }
+    LinkIn: {
+      link: "https://linkedin.com/in/alanqicao",
+      text: "MyLinkedin",
+      icon: <LinkedInIcon />,
+    },
+  },
 
-  }
+  about:
+    "I’ve always loved to build things. Still do. To me, building violins and building software are the same — I get the chance to mold a block of wood or a line of code from the ground up into something intricate and beautiful. When it comes to debugging code or restoring violins, using creative ways to solve problems and thinking outside the box is key.\n\n\nsomting else",
+  experiences:[
+    {
+      title:'Work 1',
+      date:'2010 - Present',
+      description:'i woerja sdlkjf nasdfa sdfsd sdf werw sdfs sdfsd',
 
+    },
+    {
+      title:'Work 2',
+      date:'2018 - 2010',
+      description:'i woerja sdlkjf nasdfa sdfsd sdf werw sdfs sdfsd',
 
-}
+    },
+    {
+      title:'Work 3',
+      date:'Aug 2015 - Sep 2018',
+      description:'i woerja sdlkjf nasdfa sdfsd sdf werw sdfs sdfsd',
+
+    },
+  ],
+  educations:[
+    {
+      title:'school 1',
+      date:'2010 - Present',
+      description:'i woerja sdlkjf nasdfa sdfsd sdf werw sdfs sdfsd',
+
+    },
+    {
+      title:'school 2',
+      date:'2018 - 2010',
+      description:'i woerja sdlkjf nasdfa sdfsd sdf werw sdfs sdfsd',
+
+    },
+    {
+      title:'school 3',
+      date:'Aug 2015 - Sep 2018',
+      description:'i woerja sdlkjf nasdfa sdfsd sdf werw sdfs sdfsd',
+
+    },
+  ],
+
+  services:[
+    {
+      title:'web Dev',
+      description:'I have been a web developer for 2 years',
+      icon:<WebOutlined/>,
+    },
+    {
+      title:'Branding Identity',
+      description:'I have been a web developer for 2 years',
+      icon:<WebOutlined/>,
+    },
+    {
+      title:'hand made violin ',
+      description:'I have been a web developer for 2 years',
+      icon:<WebOutlined/>,
+    },
+  
+  ],
+
+  skills:[
+    {
+      title:'FRONT-END',
+      description:[
+        "ReactJS",
+        "JavaScript",
+        "Bootstrap",
+        "Material UI",
+      ],
+    },
+    {
+      title:'BACK-END',
+      description:[
+        "Ruby",
+        "JAVA",
+        "C++",
+      ],
+    },
+    {
+      title:'DATABASES',
+      description:[
+        "Firebase",
+        "AWS",
+        "MYSQL",
+        "Postgres",
+
+      ],
+    },
+    {
+      title:'SOURCE CONTROL',
+      description:[
+        "Git",
+        "GitHub",
+        "SCRUM/Agile",
+      ],
+    },
+  ],
+
+  };
